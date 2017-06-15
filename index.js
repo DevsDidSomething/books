2623ee75c588906cf6a3f2dfc46528f636c4f9ee
Kate Ray <email@kateray.net>
2017-06-15 10:50:48 -0400

    Listen to heroku port

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.get('*', (req, res) => {
 });
 
 models.sequelize.sync().then( () => {
-  app.listen(3000, () => {
+  app.listen((process.env.PORT || 3000), () => {
     console.log('App listening on port 3000!')
   });
 });
